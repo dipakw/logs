d6a6780e93ce6fa84e1d73d866fcf073ba46b07d
Dipak Acharya <dev.dipak21@gmail.com>
2025-11-24 18:26:36 +0545

    Update test

--- logs_test.go
+++ logs_test.go
@@ -44,12 +44,56 @@ func TestMust(t *testing.T) {
 		},
 	})
 
+	logger.Wrn("This should not be logged")
 	logger.Must(Info, "Must information")
 	logger.Mustf(Info, "Must formatted %s", "information")
 	logger.Must(Warn, "Must warning")
 	logger.Mustf(Warn, "Must formatted %s", "warning")
 	logger.Must(Error, "Must error")
 	logger.Mustf(Error, "Must formatted %s", "error")
+
+	fmt.Println()
+}
+
+func TestLog(t *testing.T) {
+	logger := New(&Config{
+		Allow: All,
+		Outs: []*Out{
+			{
+				Target: os.Stdout,
+				Color:  true,
+			},
+		},
+	})
+
+	logger.Log(Info, "Log information")
+	logger.Logf(Info, "Log formatted %s", "information")
+	logger.Log(Warn, "Log warning")
+	logger.Logf(Warn, "Log formatted %s", "warning")
+	logger.Log(Error, "Log error")
+	logger.Logf(Error, "Log formatted %s", "error")
+	logger.Log(Write, "Log write")
+	logger.Logf(Write, "Log formatted %s", "write")
+
+	fmt.Println()
+}
+
+func TestWrite(t *testing.T) {
+	logger := New(&Config{
+		Allow: All,
+
+		Outs: []*Out{
+			{
+				Target: os.Stdout,
+				Color:  true,
+			},
+		},
+	})
+
+	logger.Write("Write information")
+	logger.Writef("Write formatted %s", "information")
+
+	fmt.Println()
 }
 
 func TestBefore(t *testing.T) {
@@ -79,4 +123,26 @@ func TestBefore(t *testing.T) {
 	logger.Wrnf("Before formatted %s", "warning")
 	logger.Err("Before error")
 	logger.Errf("Before formatted %s", "error")
+
+	fmt.Println()
+}
+
+func TestTypeFromString(t *testing.T) {
+	logger := New(&Config{
+		Allow: All,
+
+		Outs: []*Out{
+			{
+				Target: os.Stdout,
+				Color:  true,
+			},
+		},
+	})
+
+	logger.Log(T("info"), "Info log")
+	logger.Log(T("warn"), "Warn log")
+	logger.Log(T("error"), "Error log")
+	logger.Log(T("invalid"), "Invalid log")
+
+	fmt.Println()
 }
